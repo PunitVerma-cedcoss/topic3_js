f772687dcdce161d9c0adec5af85c1d8c48ab3b4
= <=>
2022-02-04 10:28:52 +0530

    js outputs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-function bulb_on(){
+function bulb_on() {
     document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulbon.gif";
 }
-function bulb_off(){
+function bulb_off() {
     document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulboff.gif";
+}
+function change_me() {
+    document.getElementById("change_p").innerHTML = "i am changed";
 }
