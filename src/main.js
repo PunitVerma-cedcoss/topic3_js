05cc376d19bf77af289387f472b3060c02c7d240
= <=>
2022-02-04 10:19:32 +0530

    js src manipulation

---
+++ src/main.js
@@ -0,0 +1,6 @@
+function bulb_on(){
+    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+function bulb_off(){
+    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulboff.gif";
+}
